ca4cb17b9851d7a1819086e6d8161372d926eaa4
rlcyyg1108 <122507625+rlcyyg1108@users.noreply.github.com>
2025-02-10 01:53:54 +0800

    add

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# planet-cyber
+planet-cyber
+赛博星，以配方合并和简化自动化为主题
+此模组目前处于早期开发状态，这也是我的第一个模组，我正在学习中
+https://mods.factorio.com/mod/planet-cyber
---
+++ data.lua
@@ -0,0 +1,25 @@
+data:extend(
+{
+  {
+    type = "item-group",
+    name = "rlcyyg",
+    order = "aa",
+    icon = "__rlcyyg_1_mod__/graphics/planet-cyber.png",
+    icon_size = 128,
+  },
+  {
+    type = "item-subgroup",
+    name = "rlcyyg1",
+    group = "rlcyyg",
+    order = "a"
+  },
+})
+data:extend(
+{
+  type = "item",
+  name = "planet-cyber",
+  icon = "__rlcyyg_1_mod__/graphics/planet-cyber.png",
+  subgroup = "rlcyyg1",
+  order = "a",
+  stack_size = 100,
+})
