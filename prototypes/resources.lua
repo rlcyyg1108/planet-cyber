126c2466d88b7ba1e2934f2c48414f68a2105101
rlcyyg1108 <yzy_0920@qq.com>
2025-05-19 04:50:56 +0800

    瓶子终于写完了

--- prototypes/resources.lua
+++ prototypes/resources.lua
@@ -19,7 +19,7 @@ data:extend
   {
     type = "resource",
     name = "rlcpc-ore",
-    icon = "__planet-cyber__/graphics/icons/planet-cyber.png",
+    icon = "__planet-cyber__/graphics/icons/ores.png",
     flags = {"placeable-neutral"},
     order = "a-b-h",
     map_color = color_blue,
